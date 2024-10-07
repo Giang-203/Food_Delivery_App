94d0c57a462a15923c9819b8efa087eaf5bc1a6b
Giang-203 <giangle74203@gmail.com>
2024-10-07 13:18:22 +0700

    Giang-203

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double  screenHeight = Get.context!.height;
+  static double  screenWidth = Get.context!.width;
+
+  static double pageViewContainer = screenHeight/3.66;
+  static double pageView = screenHeight/2.51;
+  static double pageViewTextContainer = screenHeight/6.71;
+
+  static double height10 = screenHeight/80.51;
+  static double height15 = screenHeight/53.67;
+  static double height20 = screenHeight/40.25;
+  static double height30 = screenHeight/26.83;
+  static double height45 = screenHeight/17.9;
+
+
+  static double width10 = screenHeight/80.51;
+  static double width15 = screenHeight/53.67;
+  static double width20 = screenHeight/40.25;
+  static double width30 = screenHeight/26.83;
+  static double width45 = screenHeight/17.9;
+
+  static double font10 = screenHeight/80.51;
+  static double font20 = screenHeight/40.25;
+
+  static double radius15 = screenHeight/53.67;
+  static double radius20 = screenHeight/40.25;
+  static double radius30 = screenHeight/26.83;
+  
+  static double iconSize24 = screenHeight/33.55;
+
+  static double listViewImgSize = screenWidth/3.27;
+  static double listViewTextContSize = screenWidth/3.927;
+
+
+
+}
---
+++ lib/widgets/icon_and_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IconAndTextWidget extends StatelessWidget {
+  const IconAndTextWidget({super.key, required IconData icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
